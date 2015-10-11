5b54a6ac0bce94f543071ab16c619392f9be909c
Mark Wilson <mark@warkmilson.com>
2015-10-11 12:47:21 -0700

    Handle NS conn errors; clean up display config

--- bg_in_menubar.py
+++ bg_in_menubar.py
@@ -1,18 +1,40 @@
 import os
+import sys
 from ConfigParser import ConfigParser
 from datetime import datetime
-from dateutil.parser import parser
-from dateutil.tz import tzlocal
 
 import requests
 import rumps
+import simplejson
+from dateutil.parser import parser
+from dateutil.tz import tzlocal
 
 date_parser = parser()
 
-NIGHTSCOUT_URL = '/api/v1/entries.json'
-UPDATE_FREQUENCY = 20
+NIGHTSCOUT_URL = '/api/v1/entries.json?count=100'
+UPDATE_FREQUENCY_SECONDS = 20
 MAX_SECONDS_TO_SHOW_DELTA = 600
 HISTORY_LENGTH = 3
+MAX_BAD_REQUEST_ATTEMPTS = 3
+
+################################################################################
+# Display options
+
+MENUBAR_TEXT = "{sgv} ({direction}) [{time_ago}]"
+MENU_ITEM_TEXT = "{sgv} [{time_ago}]"
+
+def time_ago(date_str):
+    seconds = int((datetime.now().replace(tzinfo=tzlocal()) - date_parser.parse(date_str)).total_seconds())
+    if seconds >= 3600:
+        return "%s hr" % (seconds / 3600)
+    elif seconds >= 60:
+        return "%s min" % (seconds / 60)
+    else:
+        return "%s sec" % seconds
+
+################################################################################
+
+class NightscoutException(Exception): pass
 
 class NightscoutConfig(object):
     FILENAME = 'config'
@@ -36,53 +58,16 @@ class NightscoutConfig(object):
         with open(self.config_path, 'w') as f:
             self.config.write(f)
 
-def get_bg_history_json():
-    return filter(
-        lambda e: e['type'] == 'sgv',
-        requests.get(config.get_host() + NIGHTSCOUT_URL).json()
-    )
+def update_menu(title, items):
+    app.title = title
+    app.menu.clear()
+    app.menu.update(items + last_updated_menu_items() + post_history_menu_options() + [app.quit_button])
 
-def get_menubar_text(bgs):
-    last, second_to_last = bgs[0:2]
-    if (date_parser.parse(last['dateString']) - date_parser.parse(second_to_last['dateString'])).seconds <= MAX_SECONDS_TO_SHOW_DELTA:
-        direction = ('+' if last['sgv'] >= second_to_last['sgv'] else '') + str(last['sgv'] - second_to_last['sgv'])
-    else:
-        direction = '?'
-    return "{sgv} ({direction}) [{time_ago}]".format(
-        sgv=last['sgv'],
-        direction=direction,
-        time_ago=pretty_time_ago(last['dateString']),
-    )
-
-def get_history_menu_items(bgs):
+def last_updated_menu_items():
     return [
-        '{sgv} [{time_ago}]'.format(
-            sgv=e['sgv'],
-            time_ago=pretty_time_ago(e['dateString'],
-        ))
-        for e in bgs[1:HISTORY_LENGTH + 1]
+        None,
+        "Updated %s" % datetime.now().strftime("%a %H:%M %p"),
     ]
-
-def pretty_time_ago(date_str):
-    seconds = (datetime.now().replace(tzinfo=tzlocal()) - date_parser.parse(date_str)).seconds
-    if seconds >= 3600:
-        return "%s hr" % (seconds / 3600)
-    elif seconds >= 60:
-        return "%s min" % (seconds / 60)
-    else:
-        return "%s sec" % seconds
-
-@rumps.timer(UPDATE_FREQUENCY)
-def update_data(sender):
-    try:
-        bgs = get_bg_history_json()
-        app.title = get_menubar_text(bgs)
-        app.menu.clear()
-        app.menu.update(get_history_menu_items(bgs) + post_history_menu_options() + [app.quit_button])
-    except requests.exceptions.RequestException:
-        app.title = "<Can't reach nightscout!>"
-        app.menu.clear()
-        app.menu.update(post_history_menu_options() + [app.quit_button])
 
 def post_history_menu_options():
     return [
@@ -90,6 +75,66 @@ def post_history_menu_options():
         rumps.MenuItem('Configuration', callback=configuration_window),
         None,
     ]
+
+def get_entries(retries=0, last_exception=None):
+    if retries >= MAX_BAD_REQUEST_ATTEMPTS:
+        print "Retried too many times: %s" % last_exception
+        raise NightscoutException(last_exception)
+
+    try:
+        resp = requests.get(config.get_host() + NIGHTSCOUT_URL)
+    except requests.exceptions.ConnectionError, e:
+        return get_entries(retries + 1, "Connection error")
+
+    if resp.status_code != 200:
+        return get_entries(retries + 1, "Nightscout returned status %s" % resp.status_code)
+
+    try:
+        arr = resp.json()
+        if type(arr) == list and (len(arr) == 0 or type(arr[0]) == dict):
+            return arr
+        else:
+            return get_entries(retries + 1, "Nightscout returned bad data")
+    except simplejson.scanner.JSONDecodeError:
+        return get_entries(retries + 1, "Nightscout returned bad JSON")
+
+def filter_bgs(entries):
+    return filter(lambda e: e['type'] == 'sgv', entries)
+
+def get_menubar_text(entries):
+    bgs = filter_bgs(entries)
+    last, second_to_last = bgs[0:2]
+    if (date_parser.parse(last['dateString']) - date_parser.parse(second_to_last['dateString'])).total_seconds() <= MAX_SECONDS_TO_SHOW_DELTA:
+        direction = ('+' if last['sgv'] >= second_to_last['sgv'] else '') + str(last['sgv'] - second_to_last['sgv'])
+    else:
+        direction = '?'
+    return MENUBAR_TEXT.format(
+        sgv=last['sgv'],
+        direction=direction,
+        time_ago=time_ago(last['dateString']),
+    )
+
+def get_history_menu_items(entries):
+    return [
+        MENU_ITEM_TEXT.format(
+            sgv=e['sgv'],
+            time_ago=time_ago(e['dateString'],
+        ))
+        for e in filter_bgs(entries)[1:HISTORY_LENGTH + 1]
+    ]
+
+@rumps.timer(UPDATE_FREQUENCY_SECONDS)
+def update_data(sender):
+    try:
+        entries = get_entries()
+    except NightscoutException, e:
+        update_menu("<Can't connect to Nightscout!>", [e.message])
+    else:
+        try:
+            update_menu(get_menubar_text(entries), get_history_menu_items(entries))
+        except Exception, e:
+            print "Error parsing Nightscout data: %s %s " % (e, simplejson.dumps(entries))
+            update_menu("<Bad Nightscout data!>", [])
 
 def configuration_window(sender):
     window = rumps.Window(
@@ -105,7 +150,9 @@ def configuration_window(sender):
         update_data(None)
 
 if __name__ == "__main__":
-    app = rumps.App('Nightscout Menubar', title='loading...')
-    app.menu = ['loading...'] + post_history_menu_options()
+    if len(sys.argv) > 1 and sys.argv[1] == '--debug':
+        rumps.debug_mode(True)
+    app = rumps.App('Nightscout Menubar', title='<Connecting to Nightscout...>')
+    app.menu = ['connecting...'] + post_history_menu_options()
     config = NightscoutConfig(app.name)
     app.run()
